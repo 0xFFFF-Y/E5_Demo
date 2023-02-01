394251d7c671743c0bf3a752509ef54707c420c2
0xFFFF-Y <72119877+0xFFFF-Y@users.noreply.github.com>
2023-02-01 20:49:20 +0530

    Add files via upload

---
+++ Button_ard/Button_ard.ino
@@ -0,0 +1,40 @@
+
+const int startPin = 2;  
+const int countPin = 0;
+const int resetPin = 1;
+const int ledPin = 13; 
+int startButton =0, countButton =0, resetButton=0;
+int counter = 0;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(startPin, INPUT); 
+  pinMode(countPin, INPUT); 
+  pinMode(resetPin, INPUT);  
+}
+
+void loop() {
+countButton = digitalRead(countPin);
+if (countButton == HIGH)
+{
+ counter = counter+1;
+}
+
+resetButton = digitalRead(resetPin);
+if (resetButton == HIGH)
+{
+counter = 0;
+}
+startButton = digitalRead(startPin);
+
+if (startButton == HIGH)
+{
+	for (int blink=0; blink<5; blink++)
+{
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  counter = counter-1;
+}
+}
+}
